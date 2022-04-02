ff67c985dbf4fbdf258eb8cb8070c73b80603d17
Darya <allexxika@yandex.ru>
2022-04-02 16:50:33 +0300

    Хард 1 сделан

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -10,23 +10,25 @@ public class Main {
         int d = 15;
         int e = 9;
         int result = a * (b + (c - d * e));
-        System.out.print (" Значение выражения: " + result);
+        System.out.println (" Значение выражения: " + result);
         int sres = -1 * result;
-        System.out.print (" Обратное значение выражения: " + sres);
+        System.out.println (" Обратное значение выражения: " + sres);
 
     //Задание 7
         int A = 5;
         int B = 7;
-        int A1 = B;
-        System.out.print (" Значение a было: " + A + " Стало:" + A1);
-        int B1 = A;
-        System.out.print (" Значение b было: " + B + " Стало:" + B1);
+        System.out.println (" Значение a было: " + A);
+        System.out.println (" Значение b было: " + B);
+        A = A * B / A;
+        B = A - 2;
+        System.out.println (" Значение a стало: " + A);
+        System.out.println (" Значение b стало: " + B);
 
         //Задание 8
         int all = 789;
         int mid = all % 100;
         int midle = mid / 10;
-        System.out.print (" Первичное число: " + all + " Средняя цифра:" + midle);
+        System.out.println (" Первичное число: " + all + " Средняя цифра:" + midle);
 
     }
 }
